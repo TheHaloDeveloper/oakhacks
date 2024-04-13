07611a5993b117d15d0c469da9e40c1b399a40e4
Halo <anye.dev.meltdown@gmail.com>
2024-04-13 10:26:54 -0700

    blink effect with timelinemax

--- main.js
+++ main.js
@@ -5,6 +5,7 @@
 let scene = new THREE.Scene()
 let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000)
 let renderer = new THREE.WebGLRenderer({antialias: true});
+let tl = new TimelineMax();
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('scene').appendChild(renderer.domElement)
@@ -23,8 +24,7 @@ function render(){
 
 render();
 
-function onDocumentLoad() {
-    
-}
-
-document.addEventListener('load', onDocumentLoad, false);
+setInterval(function() {
+    tl.to(sphereMesh.material, 0.5, {opacity: 0, ease: "none"})
+    tl.to(sphereMesh.material, 0.5, {opacity: 1, ease: "none"})
+}, 10000)
