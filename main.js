f7dde28740bc7782362c2f0a8dde4080a491d3bf
Halo <anye.dev.meltdown@gmail.com>
2024-04-13 12:30:16 -0700

    home screen animations

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ let renderer = new THREE.WebGLRenderer({antialias: true});
 let tl = new TimelineMax();
 let textureLoader = new THREE.TextureLoader()
 
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('scene').appendChild(renderer.domElement)
 
@@ -16,17 +17,27 @@ let sphereGeo = new THREE.SphereGeometry(3, 32, 16)
 let texture = textureLoader.load('images/texture.jpeg');
 texture.wrapS = THREE.RepeatWrapping;
 texture.wrapT = THREE.RepeatWrapping;
-texture.magFilter = THREE.NearestFilter
-texture.repeat.set(2, 1);
+texture.magFilter = THREE.LinearFilter
+texture.repeat.set(1, 1);
 let sphereMat = new THREE.MeshBasicMaterial({color: 0xff0000, transparent: true, map: texture});
 let sphereMesh = new THREE.Mesh(sphereGeo, sphereMat);
 
 scene.add(sphereMesh)
 camera.position.z = 4;
 
+let rotation = {
+    x: 0.001,
+    y: 0.001,
+    z: 0.001
+};
+
 function render(){
     requestAnimationFrame(render)
-    renderer.render(scene,camera)
+    renderer.render(scene, camera)
+
+    sphereMesh.rotation.x += rotation.x;
+    sphereMesh.rotation.y += rotation.y;
+    sphereMesh.rotation.z += rotation.z;
 }
 
 render();
@@ -47,13 +58,51 @@ function blink() {
     tl.to(document.getElementById("blocker").style, 0.5, {opacity: 0, ease: "none"})
 }
 
-window.addEventListener('resize', onWindowResize, false);
-
 function onWindowResize(){
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
+let countContainer = document.getElementById("count")
+let usernameInput = document.getElementById("usernameInput")
+
+document.getElementById("playButton").addEventListener("click", function(){
+    let username = usernameInput.value
+    if(username == ''){
+        username = 'Player'
+    }
+    stopBlink();
+    $("#main").fadeOut(1000, function(){
+        tl.to(camera.position, 2, {z: 15, ease: "bounce.out"})
+        rotation.x = 0.01;
+        rotation.y = 0.01;
+        rotation.z = 0.01;
+        setTimeout(function(){
+            countContainer.style.display = "block";
+            setTimeout(function(){
+                countContainer.innerHTML = "2";
+                setTimeout(function(){
+                    countContainer.innerHTML = "1"
+                    setTimeout(function(){
+                        countContainer.style.display = "none"
+                        rotation.y = 0.1;
+                        tl.to(camera.position, 2, {z: 0})
+                        setTimeout(function(){
+                            document.getElementById('blocker').style.opacity = 1;
+                            scene.remove(sphereMesh)
+                            renderer.setClearColor(0x87CEEB)
+                            setTimeout(function(){
+                                $("#blocker").fadeOut(1000)
+                            }, 1000)
+                        }, 2000);
+                    }, 500)
+                }, 500)
+            }, 500)
+        }, 1000)
+    })
+})
+
+window.addEventListener('resize', onWindowResize, false);
 window.addEventListener('focus', startBlink);    
 window.addEventListener('blur', stopBlink);
