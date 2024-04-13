43367bf901aa7daf237272969630bd2dbc53e5fa
Halo <anye.dev.meltdown@gmail.com>
2024-04-13 10:19:37 -0700

    sphere added

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
+/**
+ * @author Anay Mittal <anayttal@gmail.com>
+ */
+
 let scene = new THREE.Scene()
 let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000)
 let renderer = new THREE.WebGLRenderer({antialias: true});
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('scene').appendChild(renderer.domElement)
-renderer.setClearColor(0x87CEEB);
+
+let sphereGeo = new THREE.SphereGeometry(3, 32, 16)
+let sphereMat = new THREE.MeshNormalMaterial({transparent: true, opacity: 1});
+let sphereMesh = new THREE.Mesh(sphereGeo, sphereMat);
+
+scene.add(sphereMesh)
+camera.position.z = 4;
 
 function render(){
     requestAnimationFrame(render)
@@ -13,3 +23,8 @@ function render(){
 
 render();
 
+function onDocumentLoad() {
+    
+}
+
+document.addEventListener('load', onDocumentLoad, false);
