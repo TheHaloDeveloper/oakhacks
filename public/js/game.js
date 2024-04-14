a78b0eb7b0cd99b81ecd461fbfb8d92cad613489
Halo <anye.dev.meltdown@gmail.com>
2024-04-13 17:11:12 -0700

    Node.js refactor + working chat

---
+++ public/js/game.js
@@ -0,0 +1,167 @@
+/**
+ * @author Anay Mittal <anayttal@gmail.com>
+ */
+
+let username = new URLSearchParams(window.location.search).get("username")
+history.replaceState({}, document.title, window.location.pathname);
+
+//Chat
+var socket = io();
+
+let messages = document.getElementById('messages');
+let form = document.getElementById('chatForm');
+let input = document.getElementById('chatInput');
+let chatPanel = document.getElementById('chatPanel');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    if (input.value) {
+      socket.emit('chat message', [username, input.value]);
+      input.value = '';
+    }
+});
+
+socket.on('chat message', function(msg) { 
+    var item = document.createElement('li'); 
+    item.textContent = `[${msg[0]}]: ${msg[1]}`;
+    messages.appendChild(item);
+});
+
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100000);
+let renderer = new THREE.WebGLRenderer({antialias: true});
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x87CEEB);
+document.getElementById("scene").appendChild(renderer.domElement);
+
+camera.position.set(0, -22, 13);
+camera.rotation.set(1, 0, 0);
+camera.up.set(0, 0, 1);
+
+let textureLoader = new THREE.TextureLoader()
+
+let skyGeometry = new THREE.SphereGeometry(50000, 64, 32);
+let skyMaterial = new THREE.MeshBasicMaterial({map: textureLoader.load('assets/images/skybox.jpg'), side: THREE.BackSide})
+let sky = new THREE.Mesh(skyGeometry, skyMaterial);
+
+sky.name = 'Sky';
+scene.add(sky);
+
+let labelRenderer = new THREE.CSS2DRenderer();
+labelRenderer.setSize( window.innerWidth, window.innerHeight );
+labelRenderer.domElement.style.position = 'absolute';
+labelRenderer.domElement.style.top = '0px';
+labelRenderer.domElement.style.pointerEvents = 'none';
+document.getElementById('scene').appendChild(labelRenderer.domElement);
+
+//Controls
+let controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.maxDistance = 1000
+
+//Player
+let cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+let cubeMaterial = new THREE.MeshLambertMaterial({color: 0xff0000});
+let cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+cube.rotation.z = Math.PI / 2;
+cube.position.z = 0.5;
+scene.add(cube);
+
+const text = document.createElement('div');
+text.className = 'label';
+text.style.fontSize = "20px"
+text.style.fontFamily = "Verdana"
+text.textContent = username;
+
+const label = new THREE.CSS2DObject(text);
+scene.add(label)
+
+//Map
+let gltfLoader = new THREE.GLTFLoader();
+gltfLoader.load('assets/models/map.glb', function(gltf){
+    gltf.scene.position.set(80, 0, -2.5);
+    gltf.scene.rotation.x = Math.PI / 2;
+    gltf.scene.scale.set(15, 15, 15);
+    scene.add(gltf.scene);
+})
+
+//Ground
+let groundGeometry = new THREE.BoxGeometry(100000, 100000, 1);
+let groundMaterial = new THREE.MeshBasicMaterial({color: 0xFFFF91});
+let ground = new THREE.Mesh(groundGeometry, groundMaterial);
+
+ground.position.z = -3;
+scene.add(ground);
+
+//Lights
+let light = new THREE.SpotLight(0xffffff, 3);
+light.position.set(50, 0, 100);
+scene.add(light);
+
+let light2 = new THREE.SpotLight(0xffffff, 3);
+light2.position.set(-50, 0, 100);
+scene.add(light2);
+
+let keyboard = {};
+let player = {
+    speed: 0.1
+}
+
+function player_movement() {
+    if(keyboard[37]){ //left arrow key
+        cube.rotation.z += Math.PI * 0.01;
+    }
+
+    if(keyboard[39]){ //right arrow key
+        cube.rotation.z -= Math.PI * 0.01;
+    }
+
+    if(keyboard[87]){ //W key
+        cube.position.x += Math.cos(cube.rotation.z) * player.speed;
+        cube.position.y += Math.sin(cube.rotation.z) * player.speed;
+        
+        camera.position.x += Math.cos(cube.rotation.z) * player.speed;
+        camera.position.y += Math.sin(cube.rotation.z) * player.speed;
+    }
+
+    if(keyboard[83]){ //S key
+        cube.position.x -= Math.cos(cube.rotation.z) * player.speed;
+        cube.position.y -= Math.sin(cube.rotation.z) * player.speed;
+
+        camera.position.x -= Math.cos(cube.rotation.z) * player.speed;
+        camera.position.y -= Math.sin(cube.rotation.z) * player.speed;
+    }
+
+    controls.target.copy(cube.position);
+}
+
+function onWindowResize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    labelRenderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function render(){
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
+    labelRenderer.render(scene, camera);
+    label.position.set(cube.position.x, cube.position.y, cube.position.z + 1);
+
+    player_movement();
+}
+
+function keyDown(e) {
+    keyboard[e.keyCode] = true;
+}
+
+function keyUp(e) {
+    keyboard[e.keyCode] = false;
+}
+
+window.addEventListener('keydown', keyDown)
+window.addEventListener('keyup', keyUp)
+window.addEventListener('resize', onWindowResize, false);
+
+render();
---
+++ public/js/main.js
@@ -0,0 +1,117 @@
+/**
+ * @author Anay Mittal <anayttal@gmail.com>
+ */
+
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000)
+let renderer = new THREE.WebGLRenderer({antialias: true});
+let tl = new TimelineMax();
+let textureLoader = new THREE.TextureLoader()
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById('scene').appendChild(renderer.domElement)
+
+let sphereGeo = new THREE.SphereGeometry(3, 32, 16)
+
+let texture = textureLoader.load('assets/images/texture.jpeg');
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+texture.magFilter = THREE.LinearFilter
+texture.repeat.set(1, 1);
+let sphereMat = new THREE.MeshBasicMaterial({color: 0xff0000, transparent: true, map: texture});
+let sphereMesh = new THREE.Mesh(sphereGeo, sphereMat);
+
+scene.add(sphereMesh)
+camera.position.z = 4;
+
+let rotation = {
+    x: 0.001,
+    y: 0.001,
+    z: 0.001
+};
+let introOver = false;
+
+function render(){
+    requestAnimationFrame(render)
+    renderer.render(scene, camera)
+
+    if(introOver == false){
+        sphereMesh.rotation.x += rotation.x;
+        sphereMesh.rotation.y += rotation.y;
+        sphereMesh.rotation.z += rotation.z;
+    }
+}
+
+render();
+
+let blinkInterval = window.setInterval(blink, 10000);
+
+function startBlink() {
+    blinkInterval = window.setInterval(blink, 10000);
+}
+function stopBlink() {
+    window.clearInterval(blinkInterval);
+}
+
+document.getElementById("blocker").style.opacity = 0;
+
+function blink() {
+    tl.to(document.getElementById("blocker").style, 0.5, {opacity: 1, ease: "none"})
+    tl.to(document.getElementById("blocker").style, 0.5, {opacity: 0, ease: "none"})
+}
+
+function onWindowResize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+let countContainer = document.getElementById("count")
+let usernameInput = document.getElementById("usernameInput")
+
+document.getElementById("playButton").addEventListener("click", function(){
+    let username = document.getElementById("usernameInput").value
+    if(username == ''){
+        username = 'Player'
+    }
+    stopBlink();
+    $("#main").fadeOut(1000, function(){
+        tl.to(camera.position, 2, {z: 15, ease: "bounce.out"})
+        rotation.x = 0.01;
+        rotation.y = 0.01;
+        rotation.z = 0.01;
+        setTimeout(function(){
+            countContainer.style.display = "block";
+            setTimeout(function(){
+                countContainer.innerHTML = "2";
+                setTimeout(function(){
+                    countContainer.innerHTML = "1"
+                    setTimeout(function(){
+                        countContainer.style.display = "none"
+                        rotation.y = 0.1;
+                        tl.to(camera.position, 2, {z: 0})
+                        setTimeout(function(){
+                            document.getElementById('blocker').style.backgroundColor = "rgb(45, 45, 45)";
+                            document.getElementById('blocker').style.display = "block";
+                            document.getElementById('blocker').style.opacity = 1;
+                            document.getElementById('loading').style.display = "block";
+                            introOver = true;
+                            scene.remove(sphereMesh)
+                            setTimeout(function(){
+                                $("#blocker").fadeOut(1000)
+                                $("#loading").fadeOut(1000)
+                                setTimeout(function(){
+                                    window.location.href = `../game.html?username=${username}`
+                                }, 1000)
+                            }, 5000)
+                        }, 1500);
+                    }, 500)
+                }, 500)
+            }, 500)
+        }, 1000)
+    })
+})
+
+window.addEventListener('resize', onWindowResize, false);
+window.addEventListener('focus', startBlink);    
+window.addEventListener('blur', stopBlink);
